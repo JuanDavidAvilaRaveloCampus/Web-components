917c20b8dd59f037e772a3bcc800e259714a57e7
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-05-03 09:36:00 -0500

    feat: :tada: Components
    todo está bonito, solo toca hacer las planas de js

---
+++ main.js
@@ -0,0 +1,8 @@
+import myHeader from "./Components/myHeader/my-header.js";
+import mySecction from "./Components/mySecction/my-secction.js";
+import myNav from "./Components/myNav/my-nav.js";
+import mySelecction from "./Components/mySelection/my-selection.js";
+import myFooter from "./Components/myFooter/my-footer.js";
+
+
+
