1a3a0592ce778b0df2097fe8e9a5a9a1898533ac
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-05-04 08:48:42 -0500

    fix: :bug: pinta
    ya pinta :p

---
+++ Components/myHeader/my-header.js
@@ -0,0 +1,19 @@
+let pathName = new URL(import.meta.url).pathname;
+let name = pathName.split('/').pop().replace('.js','');
+
+export default class myHeader extends HTMLElement {
+    static async Components(){
+        return await ( await fetch(pathName.replace('.js','.html'))).text();
+    }
+
+    constructor(){
+        super();    
+        this.attachShadow({ mode : 'open'});
+        Promise.resolve(myHeader.Components()).then(html=>{
+            this.shadowRoot.innerHTML = html;
+            // console.log();
+        })
+        console.log('im here beatches');
+    }
+}
+customElements.define(name, myHeader);
